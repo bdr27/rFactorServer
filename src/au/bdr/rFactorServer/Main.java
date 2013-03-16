7286dd3e84f599ffb249ab25d8e3d124ae6fffce
Brendan Russo <brendanrusso27@gmail.com>
2013-03-16 18:41:56 +1000

    Removed an unnecessary import from Main

--- src/au/bdr/rFactorServer/Main.java
+++ src/au/bdr/rFactorServer/Main.java
@@ -5,7 +5,6 @@
 package au.bdr.rFactorServer;
 
 import au.bdr.rFactorServer.GUI.MainPanel;
-import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
@@ -20,7 +19,7 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setPreferredSize(new Dimension(800,600));
         frame.setVisible(true);
-        frame.setResizable(false);
+        frame.setResizable(true);
         MainPanel mainPanel = new MainPanel();
         frame.add(mainPanel);
         frame.pack();
